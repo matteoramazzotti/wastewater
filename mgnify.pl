6aff96c003e508d3fd2749782c16ef18f015319b
Matteo Ramazzotti <matteo.ramazzotti@unifi.it>
2020-02-29 19:11:27 +0100

    Update mgnify.pl

--- mgnify.pl
+++ mgnify.pl
@@ -1,42 +1,129 @@
+#!/usr/bin/perl
+if (!$ARGV[0]) {
+print STDERR "\nUSAGE: mgnify.pl prep|down|collect [MGnifile] folder\n\n";
+print STDERR "       prep: reads the tabular file obtained form the MGnify \"Download results as CSV\" button.\n";
+print STDERR "             and creates the mgnify.links file in the specified folder\n";
+print STDERR "       down: reads the mgnify.links file form the specified folder and starts downloading\n";
+print STDERR "             taxonomic abundance tables that are accumulated in the mgnify.out file in the specified folder\n";
+print STDERR "    collect: reads the mgnify.out file in the specified folder and prepares the various mgnify.rank files\n";
+print STDERR "       grep: reads the mgnify.out filtering matching lines\n";
+print STDERR "     grepan: reads the mgnify.out file and displays tax tables of specific analyses\n\n";
+exit;
+}
+
 $mode = $ARGV[0];
-if ($mode eq "grep") {
-	open(IN,"mgnify.links");
+
+if ($mode eq "prep") { #usage: MGnify.pl prep infile folder
+	open(IN,$ARGV[1]); #the input file from MGnify 
+	#the input file derives form the default MGnify "Download results as CSV" button pressed on the alyses tab. 
+	#it forms lines that looks like this: "MGYA00087407","3.0","ERS1462490","MGYS00001338","amplicon",,"ERR1744699",
+	mkdir $ARGV[2] if (!-d $ARGV[2]); #a destination folder
+	open(OUT,">$ARGV[2]/mgnify.links");
+	#the output should be like this: MGYA00067182	MGYS00001166	https://www.ebi.ac.uk/metagenomics/api/v1/analyses/MGYA00067182/file/SRR2001789_MERGED_FASTQ_otu.tsv
+	$cnt = 0;
 	while ($line = <IN>) {
-		next if ($line =~/^#/);
+		$cnt++;
+		next if ($cnt == 1);
 		chomp $line;
-		($s,$p,undef) = split (/\t/,$line);
-		$proj{$s} = $p;
+		$line =~ s/\"//g;
+		($analysis,$version,$sample,$study,$exptype,undef,$run) = split (/,/,$line);
+		print OUT $analysis,"\t",$study,"\t","https://www.ebi.ac.uk/metagenomics/api/v1/analyses/",$analysis,"/file/",$run,"_MERGED_FASTQ_\n";
 	}
 	close IN;
-	open(IN,"mgnify.out");
+	close OUT;
+}
+
+if ($mode eq "down") { #usage: Mgnify.pl down folder
+	open(IN,"$ARGV[1]/mgnify.out.log");
 	while ($line = <IN>) {
 		chomp $line;
+		@parts = split (/\t/,$line);
+		$used{$parts[0]} = 1 if ($parts[$#parts] ne "no");
+	}
+	close IN;
+	open(IN,"$ARGV[1]/mgnify.links");
+	open(OUT,">>$ARGV[1]/mgnify.out");
+	open(LOG,">>$ARGV[1]/mgnify.out.log");
+	@case = qw /_MERGED_FASTA_SSU_OTU_TABLE_JSON.biom _MERGED_FASTQ_SSU_OTU_TABLE_JSON.biom _FASTA_SSU_OTU_TABLE_JSON.biom _FASTQ_SSU_OTU_TABLE_JSON.biom _MERGED_FASTQ_otu.tsv _FASTQ_otu.tsv _MERGED_FASTA_otu.tsv _FASTQ_otu.tsv/;
+	while ($line = <IN>) {
 		next if ($line =~ /^#/);
-		$current = $line if ($line =~ /^MGYA/);
-		print $current,"\t",$proj{$current},"\t",$line,"\n" if ($line =~ /$ARGV[1]/i);
+		chomp $line;
+#		$mode = 1;
+		#a: analysis, s: study, l: link (partial)
+		($a,$s,$l) = split (/\t/,$line);
+		$tmp = '';
+		$run = $l;
+		$run =~ s/.+\///;
+#		print STDERR "$s @ $l:";
+		print STDERR "$a @ $s: $run ";
+		print STDERR " already done.\n" if ($used{$a});
+		next if ($used{$a});
+		$end = 'no';
+		foreach $mode (0..$#case) {
+			$link = $l.$case[$mode];
+			#print STDERR "\n TESTING $link...";
+			$tmp = `wget -q -O- $link`;
+			#print STDERR "$mode." if $tmp =~ /\w/ && $tmp !~ /DOCTYPE html/;
+			if ($tmp =~ /\w/ && $tmp !~ /DOCTYPE html/) {
+				$end = $case[$mode];
+				last;
+			}
+#			<STDIN>;
+		}
+		#a parser for the biom file
+		if ($tmp =~ /\w/ && $link =~ /JSON/) {
+			@tax = ();%tax = ();
+			$tmp =~ /\"data\": \[\[(.+?)\]\]/;
+			$data = ",[$1";
+			while ($tmp =~ /{\"id\": \"(\d+)\", \"metadata\": \{\"taxonomy\": \[(.+?)\]/g) {
+				push(@tax,$1);
+				$tax{$1} = $2;
+			}
+			$cnt = 0;
+			$tmp = '';
+			while($data =~ /\[\d+,\d+,(.+?)\]/g) {
+				$abund[$cnt] = $1;
+				$tmp .= $tax[$cnt]."\t".$abund[$cnt]."\t".$tax{$tax[$cnt]}."\n";
+				$cnt++;
+			}
+		}
+		print STDERR "$end\n";		
+		print LOG "$a\t$s\t$run\t$end\n";
+		print OUT $a,"\t",$s,"\n",$tmp,"\n";
+#		<STDIN>;
+#		sleep 2;
+		print STDERR "Compressing mgnify.out...";
+		`"gzip $ARGV[1]/mgnify.out`;	
+		print STDERR "done.\n";		
 	}
 	close IN;
+	close OUT;
+	close LOG;
 }
 if ($mode eq "collect") {
-	open(IN,"mgnify.out");
+	open(IN,"zcat $ARGV[1]/mgnify.out.gz|");
 	$cnt = 0;
 	while ($line = <IN>) {
 		chomp $line;
 		next if ($line =~ /^#/);
 		if ($line =~ /^MGYA/) {
+			print STDERR $cnt{$exp},"\n" if (%exp); 
 			$exp = $line;
 			$exp{$line} = 1;
 			push @exp, $line;
 			$cnt++;
-			print STDERR "$cnt. Parsing $line\n";
+			print STDERR "$cnt. Parsing $line: ";
 		} else {
 			$line =~ /k__(.*?); p__(.*?); c__(.*?); o__(.*?); f__(.*?); g__(.*?); s__(.*?)/;
-			$k{$1} = 1;$p{$2} = 1;$c{$3} = 1;$o{$4} = 1;$f{$5} = 1;$g{$6} = 1;$s{$5." ".$6} = 1;
-			$kx{$1."@".$exp} = 1;$px{$2."@".$exp} = 1;$cx{$3."@".$exp} = 1;$ox{$4."@".$exp} = 1;$fx{$5."@".$exp} = 1;$gx{$6."@".$exp} = 1;$sx{$5." ".$6."@".$exp} = 1;
+#			print $6,"\n";
+			$k{$1} = 1;$p{$2} = 1;$c{$3} = 1;$o{$4} = 1;$f{$5} = 1;$g{$6} = 1;$s{$6." ".$7} = 1;
+			$kx{$1."@".$exp} = 1;$px{$2."@".$exp} = 1;$cx{$3."@".$exp} = 1;$ox{$4."@".$exp} = 1;$fx{$5."@".$exp} = 1;$gx{$6."@".$exp} = 1;$sx{$6." ".$7."@".$exp} = 1;
+			$cnt{$exp}++;
 		}
 #		last if $cnt > 100;
 	}
 	close IN;
+	print STDERR scalar keys %cnt," / ",scalar keys %exp," experiments with info\n";
 	print STDERR "Kingdom\t",scalar keys %k,"\n";
 	print STDERR "Phylum\t",scalar keys %p,"\n";
 	print STDERR "Class\t",scalar keys %c,"\n";
@@ -45,7 +132,7 @@ if ($mode eq "collect") {
 	print STDERR "Genus\t",scalar keys %g,"\n";
 	print STDERR "Species\t",scalar keys %s,"\n";
 
-  	open(OUT,">mgnify.kinkdom");
+  	open(OUT,">$ARGV[1]/mgnify.kingdom");
 	print OUT "Name\t",(join "\t",sort keys %exp),"\t#exp\n";
 	foreach $v (sort keys %k) {
 		print OUT $v;
@@ -59,7 +146,7 @@ if ($mode eq "collect") {
 	}
 	close OUT;
 
-  	open(OUT,">mgnify.phyla");
+  	open(OUT,">$ARGV[1]/mgnify.phyla");
 	print OUT "Name\t",(join "\t",sort keys %exp),"\t#exp\n";
 	foreach $v (sort keys %p) {
 		print OUT $v;
@@ -73,7 +160,7 @@ if ($mode eq "collect") {
 	}
 	close OUT;
 
- 	open(OUT,">mgnify.class");
+ 	open(OUT,">$ARGV[1]/mgnify.class");
 	print OUT "Name\tnexp\t",(join "\t",sort keys %exp),"\t#exp\n";
 	foreach $v (sort keys %c) {
 		print OUT $v;
@@ -87,7 +174,7 @@ if ($mode eq "collect") {
 	}
 	close OUT;
 
- 	open(OUT,">mgnify.order");
+ 	open(OUT,">$ARGV[1]/mgnify.order");
 	print OUT "Name\tnexp\t",(join "\t",sort keys %exp),"\t#exp\n";
 	foreach $v (sort keys %o) {
 		print OUT $v;
@@ -101,7 +188,7 @@ if ($mode eq "collect") {
 	}
 	close OUT;
 
- 	open(OUT,">mgnify.family");
+ 	open(OUT,">$ARGV[1]/mgnify.family");
 	print OUT "Name\tnexp\t",(join "\t",sort keys %exp),"\t#exp\n";
 	foreach $v (sort keys %f) {
 		print OUT $v;
@@ -115,7 +202,7 @@ if ($mode eq "collect") {
 	}
 	close OUT;
 
- 	open(OUT,">mgnify.genus");
+ 	open(OUT,">$ARGV[1]/mgnify.genus");
 	print OUT "Name\t",(join "\t",sort keys %exp),"\t#exp\n";
 	foreach $v (sort keys %g) {
 		print OUT $v;
@@ -129,7 +216,7 @@ if ($mode eq "collect") {
 	}
 	close OUT;
 
- 	open(OUT,">mgnify.species");
+ 	open(OUT,">$ARGV[1]/mgnify.species");
 	print OUT "Name\t",(join "\t",sort keys %exp),"\t#exp\n";
 	foreach $v (sort keys %s) {
 		print OUT $v;
@@ -144,39 +231,57 @@ if ($mode eq "collect") {
 	close OUT;
 }
 
-if ($mode eq "down") {
-	if (-e "mgnify.out") {
-		open(IN,"mgnify.out");
-		while ($line = <IN>) {
-			chomp $line;
-			$used{$line}=1 if ($line =~ /^MGYA/);
-		}
-		close IN;
-	}
-	open(IN,"mgnify.links");
-	open(OUT,">>mgnify.out");
+if ($mode eq "grep") { #this is a filter for the output of collect
+	open(IN,"$ARGV[1]/mgnify.links");
 	while ($line = <IN>) {
-		next if ($line =~ /^#/);
+		next if ($line =~/^#/);
 		chomp $line;
-		$mode = 1;
-		#s: sample, l:link,undef is the study, that is not of interest here...
-		($s,undef,$l) = split (/\t/,$line);
-		print STDERR "$s...";
-		print STDERR "done.\n" if ($used{$s});
-		next if ($used{$s});
-		$tmp = `wget -q -O- $line` if ($mode == 1);
-		print STDERR "1...";
-		DOWN:
-		$line =~ s/MERGED_// if ($mode == 2);
-		$tmp = `wget -q -O- $line` if ($mode == 2);
-		if ($tmp !~ /\w/ && $mode == 1) {
-			$mode = 2;
-			print STDERR "2...";
-			goto DOWN;
-		}
-		print OUT $s,"\n",$tmp,"\n";
-		print STDERR "\n";
-		sleep 2;
+		($s,$p,undef) = split (/\t/,$line);
+		$proj{$s} = $p;
 	}
 	close IN;
+	open(IN,"zcat $ARGV[1]/mgnify.out.gz|");
+	while ($line = <IN>) {
+		chomp $line;
+		next if ($line =~ /^#/);
+		$current = $line if ($line =~ /^MGYA/);
+		print $current,"\t",$proj{$current},"\t",$line,"\n" if ($line =~ /$ARGV[2]/i);
+	}
+	close IN;
+}
+
+if ($mode eq "grepan") { #this is a filter for the output of collect, specifically writes the tax table of a specific analysis
+	open(IN,"zcat $ARGV[1]/mgnify.out.gz|");
+	$print = 0;
+	while ($line = <IN>) {
+		next if ($line =~ /^#/);
+		$print = 1 if ($line =~ /^MGYA/ && $line =~ /$ARGV[2]/i);
+		$print = 0 if ($line =~ /^MGYA/ && $line !~ /$ARGV[2]/i);
+		print $line if ($print);
+	}
+	close IN;
+}
+
+if ($mode eq "count") { #this is a filter for the output of collect
+	open(IN,"$ARGV[1]/mgnify.links");
+	while ($line = <IN>) {
+		next if ($line =~/^#/);
+		chomp $line;
+		($s,$p,undef) = split (/\t/,$line);
+		$proj{$s} = $p;
+	}
+	close IN;
+	open(IN,"zcat $ARGV[1]/mgnify.out.gz|");
+	while ($line = <IN>) {
+		chomp $line;
+		next if ($line =~ /^#/);
+		next if ($line !~ /\w/);
+		$current = $line if ($line =~ /^MGYA/);
+		next if ($line =~ /^MGYA/);
+		$cnt{$current}++;
+	}
+	close IN;
+	foreach $s (sort {$cnt{$b} <=> $cnt{$a}} keys %cnt) {
+		print $s,"\t",$proj{$s},"\t",$cnt{$s},"\n";
+	}
 }
